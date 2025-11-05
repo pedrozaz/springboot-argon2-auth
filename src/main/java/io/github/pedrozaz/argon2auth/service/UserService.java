e05c8efa6ae034cf8f24827f38283ae6602546cf
Pedrozaz <guilhermebarb0sa@proton.me>
2025-11-05 20:13:31 -0300

    UserService logic added.

---
+++ src/main/java/io/github/pedrozaz/argon2auth/service/UserService.java
@@ -0,0 +1,23 @@
+package io.github.pedrozaz.argon2auth.service;
+
+import io.github.pedrozaz.argon2auth.model.User;
+import io.github.pedrozaz.argon2auth.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+}
