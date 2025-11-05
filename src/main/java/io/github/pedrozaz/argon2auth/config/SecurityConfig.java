9652167cd489bd7fd0f83c368cc7ac5c39aa9fc1
Pedrozaz <guilhermebarb0sa@proton.me>
2025-11-05 20:11:11 -0300

    Bouncycastle dependency and SecurityConfig added

---
+++ src/main/java/io/github/pedrozaz/argon2auth/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package io.github.pedrozaz.argon2auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/register").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder(16, 32, 1, 1 << 16, 3);
+    }
+}
