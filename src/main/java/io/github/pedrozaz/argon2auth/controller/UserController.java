b7cb1a9f4db23f2885d03fbc82fd396448a8d8e7
Pedrozaz <guilhermebarb0sa@proton.me>
2025-11-05 20:16:20 -0300

    UserController added

---
+++ src/main/java/io/github/pedrozaz/argon2auth/controller/UserController.java
@@ -0,0 +1,24 @@
+package io.github.pedrozaz.argon2auth.controller;
+
+import io.github.pedrozaz.argon2auth.model.User;
+import io.github.pedrozaz.argon2auth.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+}
